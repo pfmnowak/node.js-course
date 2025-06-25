1530853cc48912ddf48ee52dfdcba1307cb7f375
Mikołaj Nowak <pfmnowak@gmail.com>
2025-06-25 21:17:12 +0200

    feat: use pug as a template engine

--- routes/admin.js
+++ routes/admin.js
@@ -8,15 +8,21 @@ const router = express.Router();
 
 const products = [];
 
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   //   console.log("In another middleware!");
   //   res.send(
   //     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button>ADD Product</button></form>'
   //   );
 
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
+// /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
   products.push({ title: req.body.title });
   console.log(req.body);
