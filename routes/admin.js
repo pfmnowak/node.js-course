3774bf492e8c45b4f1e083b6c991578068248932
Mikołaj Nowak <pfmnowak@gmail.com>
2025-07-27 21:09:32 +0200

    feat: use ejs as a template engine, switch to mvc pattern, cleanup

--- routes/admin.js
+++ routes/admin.js
@@ -1,33 +1,15 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
 
-const rootDir = require("../util/path");
-const { title } = require("process");
+const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  //   console.log("In another middleware!");
-  //   res.send(
-  //     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button>ADD Product</button></form>'
-  //   );
-
-  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  console.log(req.body);
-  res.redirect("/");
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,29 +1,11 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
 
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-// router.get("/", (req, res, next) => {
-//   console.log("This always runs!");
-//   next();
-// });
-
-router.get("/", (req, res, next) => {
-  // console.log(adminData.products);
-  //   res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
-
-  const products = adminData.products;
-  // res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-  });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
