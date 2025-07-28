dac0770f3bdaa8a76fff6d9b421b3879a4c34464
Mikołaj Nowak <pfmnowak@gmail.com>
2025-07-28 20:53:51 +0200

    feat: use dynamic params

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,19 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+
+  Product.findById(prodId, (product) => {
+    console.log(product);
+    // res.render("shop/product-list", {
+    //   prods: products,
+    //   pageTitle: "All Products",
+    //   path: "/products",
+    // });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
