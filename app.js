e188777dfb7aed50918ba322d109613904890cde
Mikołaj Nowak <pfmnowak@gmail.com>
2025-08-03 19:10:19 +0200

    feat: create a user entity

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
 const sequelize = require('./util/database');
+
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +20,39 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+	User.findByPk(1)
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => {
+			console.error(err);
+			next(new Error('Failed to fetch user'));
+		});
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
+	// .sync({ force: true }) // Use force: true only for development to reset the database
 	.sync()
 	.then((result) => {
+		return User.findByPk(1);
+	})
+	.then((user) => {
+		if (!user) {
+			return User.create({ name: 'Mik', email: 'mik@example.com' });
+		}
+		return Promise.resolve(user);
+	})
+	.then((user) => {
 		app.listen(3000);
 	})
 	.catch((err) => {
