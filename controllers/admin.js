1702a877d0cf5279ad9dc8fac62219c9ea43ed72
Mikołaj Nowak <pfmnowak@gmail.com>
2025-09-23 17:26:10 +0200

    feat: delete products from mongo db

--- controllers/admin.js
+++ controllers/admin.js
@@ -82,16 +82,13 @@ exports.getProducts = (req, res, next) => {
 		});
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-// 	const prodId = req.body.productId;
-// 	Product.findByPk(prodId)
-// 		.then((product) => {
-// 			return product.destroy();
-// 		})
-// 		.then(() => {
-// 			res.redirect('/admin/products');
-// 		})
-// 		.catch((err) => {
-// 			console.error(err);
-// 		});
-// };
+exports.postDeleteProduct = (req, res, next) => {
+	const prodId = req.body.productId;
+	Product.deleteById(prodId)
+		.then(() => {
+			res.redirect('/admin/products');
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+};
