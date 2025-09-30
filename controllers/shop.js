75f731e339e6579c41c8b5cf638c356c05d75526
Mikołaj Nowak <pfmnowak@gmail.com>
2025-09-30 18:42:32 +0200

    feat: display cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,17 +44,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
 	req.user
 		.getCart()
-		.then((cart) => {
-			return cart
-				.getProducts()
-				.then((products) => {
-					res.render('shop/cart', {
-						path: '/cart',
-						pageTitle: 'Your Cart',
-						products: products,
-					});
-				})
-				.catch((err) => console.log(err));
+		.then((products) => {
+			res.render('shop/cart', {
+				path: '/cart',
+				pageTitle: 'Your Cart',
+				products: products,
+			});
 		})
 		.catch((err) => console.log(err));
 };
@@ -68,6 +63,7 @@ exports.postCart = (req, res, next) => {
 		})
 		.then((result) => {
 			console.log(result);
+			res.redirect('/cart');
 		})
 		.catch((err) => console.log(err));
 };
