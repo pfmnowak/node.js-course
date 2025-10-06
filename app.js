987ba13767c9a5154dc2d39ca359619a75754250
Mikołaj Nowak <pfmnowak@gmail.com>
2025-10-06 22:16:22 +0200

    feat: add User model with mongoose

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,16 +19,16 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-// User.findById('68d555a340b728d297d66eed')
-// 	.then((user) => {
-// 		req.user = new User(user.name, user.email, user.cart, user._id);
-// 		next();
-// 	})
-// 	.catch((err) => {
-// 		console.error(err);
-// 	});
-// });
+app.use((req, res, next) => {
+	User.findById('68e40ee4348c439131dd0d2f')
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+});
 
 app.use('/admin', adminRoutes);
 
