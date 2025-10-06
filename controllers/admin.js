da6b2e6e871bd3494ef6120dfa29a96cde2d96d8
Mikołaj Nowak <pfmnowak@gmail.com>
2025-10-06 20:43:59 +0200

    feat: edit and delete products with mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -62,10 +62,14 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedImageUrl = req.body.imageUrl;
 	const updatedDesc = req.body.description;
 
-	const product = new Product(updatedTitle, updatedImageUrl, updatedDesc, updatedPrice, prodId);
-
-	return product
-		.save()
+	Product.findById(prodId)
+		.then((product) => {
+			product.title = updatedTitle;
+			product.price = updatedPrice;
+			product.description = updatedDesc;
+			product.imageUrl = updatedImageUrl;
+			return product.save();
+		})
 		.then(() => {
 			res.redirect('/admin/products');
 		})
@@ -75,7 +79,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-	Product.fetchAll()
+	Product.find()
 		.then((products) => {
 			res.render('admin/products', {
 				prods: products,
@@ -90,7 +94,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.deleteById(prodId)
+	Product.findByIdAndDelete(prodId)
 		.then(() => {
 			res.redirect('/admin/products');
 		})
